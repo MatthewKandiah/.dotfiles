bc5c757be085b763fcf2604d4e8160f7b727ac5d
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-11-13 18:22:24 +0000

    tweaking

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -69,7 +69,7 @@ require('lazy').setup({
 		dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
 		build = ':TSUpdate',
 	},
-	-- { 'folke/which-key.nvim', opts = {} },
+	{ 'folke/which-key.nvim', opts = {} },
 	{
 		'numToStr/Comment.nvim',
 		opts = {},
@@ -113,8 +113,6 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
--- Undo tree
-vim.keymap.set('n', '<leader>u', ":UndotreeToggle<CR>", { desc = 'Toggle undo tree' })
 
 -- telescope config
 -- search hidden files, but still ignore .git files https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
