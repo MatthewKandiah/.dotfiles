9f7d5b02b098a77341d5872135ee00eedc3bd64e
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-11-24 18:14:52 +0000

    better commandline completions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -186,7 +186,7 @@ cmp.setup.cmdline({ '/', '?' }, {
 cmp.setup.cmdline({ ':' }, {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
-    { name = 'buffer' },
+    { name = 'cmdline' },
     { name = 'path' },
   },
 })
