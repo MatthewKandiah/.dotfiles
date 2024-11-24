4c591dddc887e95c706dc807c28852d31db6afd6
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-11-24 06:22:09 +0000

    more config tweaking - improved completion setup and lsp signature help command in insert mode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,7 +18,7 @@ vim.o.smartcase = true
 vim.wo.signcolumn = 'yes'
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = 'menuone,noselect,preview'
 vim.o.termguicolors = true
 vim.wo.linebreak = true
 vim.o.cursorline = true
@@ -61,6 +61,9 @@ require('lazy').setup({
   'folke/lua-dev.nvim',
   'folke/neodev.nvim',
   'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-cmdline',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-buffer',
   'hrsh7th/nvim-cmp',
   'L3MON4D3/LuaSnip',
   'saadparwaiz1/cmp_luasnip',
@@ -103,16 +106,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
--- move line commands, stolen from ThePrimeagen
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-vim.keymap.set('v', '<S-Up>', ":m '<-2<CR>gv=gv")
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', '<S-Down>', ":m '>+1<CR>gv=gv")
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- telescope config
 -- search hidden files, but still ignore .git files https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
@@ -141,14 +138,11 @@ vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc =
 vim.keymap.set('n', '<leader><space>', function()
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
-    previewer = false,
+    previewer = true,
   })
 end, { desc = '[ ] Fuzzily search in current buffer' })
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>s~', function()
-  require('telescope.builtin').find_files({ cwd = vim.env.HOME })
-end, { desc = '[S]earch [~] Directory' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').search_history, { desc = '[S]earch [H]istory' })
 vim.keymap.set('n', '<leader>sg', function()
   require('telescope.builtin').live_grep({ disable_coordinates = true })
@@ -173,26 +167,45 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<C-y>'] = cmp.mapping.confirm({ select = false }),
+    cmp.m
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  })
+    -- { name = 'luasnip' }, Not actually sure what completions this adds, snippets presumably? Removing to see if I notice a difference
+    { name = 'buffer' },
+    { name = 'path' },
+  }),
+})
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  },
+})
+cmp.setup.cmdline({ ':' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' },
+    { name = 'path' },
+  },
 })
 
 -- lsp config
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local on_attach = function(_, bufnr)
-  local nmap = function(keys, func, desc)
+  local lsp_keymap = function(mode, keys, func, desc)
     if desc then
-      desc = 'LSP: ' .. desc
+      desc = 'LSP: ' .. desc .. '(' .. mode .. ')'
     end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
   end
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+  local nmap = function(keys, func, desc) lsp_keymap('n', keys, func, desc) end
+  local imap = function(keys, func, desc) lsp_keymap('i', keys, func, desc) end
+
+  nmap('<leader>r', vim.lsp.buf.rename, '[R]ename')
+  nmap('<leader>c', vim.lsp.buf.code_action, '[C]ode Action')
   nmap('gd', function()
     require('telescope.builtin').lsp_definitions({ show_line = false })
   end, '[G]oto [D]efinition')
@@ -209,6 +222,7 @@ local on_attach = function(_, bufnr)
   nmap('gc', function()
     require('telescope.builtin').grep_string()
   end, '[G]rep string under [c]ursor')
+  imap('<C-h>', vim.lsp.buf.signature_help, 'LSP Signature [H]elp')
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
@@ -231,7 +245,7 @@ mason_lspconfig.setup_handlers {
     lspconfig[server_name].setup(default_lsp_options)
   end,
   lspconfig.racket_langserver.setup(default_lsp_options), -- requires racket-langserver, install with `raco pkg install racket-langserver`
-  lspconfig.zls.setup(default_lsp_options),              -- requires zls on path, used because Mason installs an outdated version
+  lspconfig.zls.setup(default_lsp_options),               -- requires zls on path, used because Mason installs an outdated version
   lspconfig.dartls.setup(default_lsp_options),
 }
 
