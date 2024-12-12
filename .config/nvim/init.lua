b45bfa13bb851e943d6bfc8a47c2e26b33ddd481
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-12-12 07:32:48 +0000

    keybind format too

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -228,6 +228,7 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+  nmap('<leader>f', vim.lsp.buf.format, "Format current buffer with LSP")
 end
 
 -- filetype for go templ
